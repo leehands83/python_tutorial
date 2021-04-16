fd08269cd04926e2ea2eab9641a97e060e24a672
nhmac <leehands83@icloud.com>
2021-04-17 08:53:32 +0900

    import tesnorflow module

--- basic_import_tensorflow.py
+++ basic_import_tensorflow.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+#git addr : https://github.com/leehands83/python_tutorial.git
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+import tensorflow as tf
 
+tf.constant(200.0)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print('tensor flow start ')
